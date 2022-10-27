36b2bd3898f3a3769039e304771a79cf1c7dfd85
ahmed-razic <ahmed.razic@gmail.com>
2022-10-27 09:35:45 +0200

    two-way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,12 +10,22 @@ import { timeout } from 'rxjs';
 })
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'FirstServer';
 
   constructor() {
     setTimeout(()=>{this.allowNewServer = true},3000)
   }
-
+  
   ngOnInit(): void {
   }
-
+  
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+  
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
